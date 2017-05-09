f4761f40c31beb8cae55f68c96703830eef718f6
hueanmy <ltmai93@gmail.com>
2017-05-09 22:04:49 +0700

    chap11

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
-User.create!(name: "Example User", email: "example@railstutorial.org",
-  password: "foobar", password_confirmation: "foobar")
+User.create!(name: "mai", email: "ltmai93@gmail.com",
+  password: "111111", password_confirmation: "111111", admin: true,
+  activated: true, activated_at: Time.zone.now)
 
-99.times do |n|
+30.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
+  email = "mai-#{n+1}@gmail.com"
+  password = "111111"
   User.create!(name:  name, email: email, password: password,
-    password_confirmation: password)
+    password_confirmation: password, activated: true,
+    activated_at: Time.zone.now)
 end
